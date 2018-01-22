ef0f3776f42ec2554049b148502cdf6059204839
sangavik <31438501+sangavik@users.noreply.github.com>
2018-01-22 16:07:42 +0530

    PROG FOR POS.C

---
+++ PROG FOR POS.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(0
+{
+int num;
+printf("enter the num\n"0;
+scanf("%d",&num);
+if(num>0)
+printf("%d is pos",num);
+elseif(num<0)
+printf("%d is neg",num);
+else
+printf(" 0 is neither pos or neg");
+}

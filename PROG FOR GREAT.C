3600e77efbf36419fccd3f57e77d1f264925878f
sangavik <31438501+sangavik@users.noreply.github.com>
2018-01-22 16:23:07 +0530

    PROG FOR GREAT.C

---
+++ PROG FOR GREAT.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter three nums\n");
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+{
+printf("\n %d is larger than %d",&a7b&c);
+}
+if((b>a)&&(b>c))
+{
+printf("\n %d is larger than %d",&b&a&c);
+}
+if((c.a>&&(c>b))
+{
+printf("\n %d is larger than %d",&c&b&a);
+}
+return 0;
+}

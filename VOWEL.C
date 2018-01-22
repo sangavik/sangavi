7c343b3cfa10c626856504eb01513d8d4ffff042
sangavik <31438501+sangavik@users.noreply.github.com>
2018-01-22 16:33:18 +0530

    VOWEL.C

---
+++ VOWEL.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch=='a'\\ch='e'\\ch='i'\\ch='o'\\ch='u'
+printf("%c is a vowel \n",ch);
+else
+return 0;
+}
